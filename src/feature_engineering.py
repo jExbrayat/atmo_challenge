45df236eff9d2bf07163cc19bf7db225e203b568
jEx <jules.exbrayat@ensimag-inp.org>
2025-01-15 15:12:15 +0100

    refactor: adapting paths to data folder tree

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,11 +3,11 @@
 import pandas as pd
 
 # Retrieve the datasets
-smh_data = pd.read_csv("data/smh_ref_micro_merged.csv")
-smh_hum_temp = pd.read_csv("data/smh_23-24_humidity_temp.csv")
+lf_data = pd.read_csv("data/intermediary_datasets/lf_ref_micro_merged.csv")
+lf_hum_temp = pd.read_csv("data/intermediary_datasets/lf_23-24_humidity_temp.csv")
 
 # Set datetime type to the date column
-for df in [smh_data, smh_hum_temp]:
+for df in [lf_data, lf_hum_temp]:
     df["date"] = pd.to_datetime(df["date"])
     df["date"] = df["date"].dt.tz_localize(None)
     # Set datetime column as index
@@ -17,11 +17,11 @@ for df in [smh_data, smh_hum_temp]:
 # Build seasonality categorical variables
 
 # Adapt pandas index type
-smh_data_datetime_idx = smh_data.index.to_period("h")
+lf_data_datetime_idx = lf_data.index.to_period("h")
 
 # Create columns indicating day and month
-smh_data["hourofday"] = smh_data_datetime_idx.hour
-smh_data["monthofyear"] = smh_data_datetime_idx.month
+lf_data["hourofday"] = lf_data_datetime_idx.hour
+lf_data["monthofyear"] = lf_data_datetime_idx.month
 
 # Define a mapping of months to seasons
 month_to_season = {
@@ -40,34 +40,34 @@ month_to_season = {
 }
 
 # Add a "season" column based on the mapping
-smh_data["season"] = smh_data["monthofyear"].map(month_to_season)
+lf_data["season"] = lf_data["monthofyear"].map(month_to_season)
 
 
 # Rename the variables
 # valeur_x stands for the reference station's measures, y for the microsensor's
-smh_data.rename(
+lf_data.rename(
     columns={"valeur_x": "reference_pm25", "valeur_y": "microsensor_pm25"}, inplace=True
 )
 # valeur_x stands for the temperature measures, y for the humidity
-smh_hum_temp.rename(
+lf_hum_temp.rename(
     columns={"valeur_x": "temperature", "valeur_y": "humidity"}, inplace=True
 )
 
 # Drop the undesired columns
-smh_data = smh_data.loc[
+lf_data = lf_data.loc[
     :, ["reference_pm25", "microsensor_pm25", "hourofday", "monthofyear", "season"]
 ]
-smh_hum_temp = smh_hum_temp.loc[:, ["temperature", "humidity"]]
+lf_hum_temp = lf_hum_temp.loc[:, ["temperature", "humidity"]]
 
 # Reorder columns
-smh_data = smh_data.loc[
-    :, [col for col in smh_data.columns if col != "reference_pm25"] + ["reference_pm25"]
+lf_data = lf_data.loc[
+    :, [col for col in lf_data.columns if col != "reference_pm25"] + ["reference_pm25"]
 ]
 
 # Merge the two datasets
-smh_merge = pd.merge(
-    smh_data, smh_hum_temp, left_index=True, right_index=True
+lf_merge = pd.merge(
+    lf_data, lf_hum_temp, left_index=True, right_index=True
 )  # inner join
 
 # Save dataset
-smh_merge.to_csv("data/smh_23-24_training_set.csv")
+lf_merge.to_csv("data/intermediary_datasets/lf_23-24_training_set.csv")
